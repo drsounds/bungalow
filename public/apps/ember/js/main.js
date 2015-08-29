33011a58a4d1ba1731354f5b76d1ff08f4505215
Alexander Forselius <drsounds@gmail.com>
2015-08-29 17:39:37 +0200

    Fixed sidebar icons

--- public/apps/ember/js/main.js
+++ public/apps/ember/js/main.js
@@ -1,5 +1,9 @@
 // Get the canvas element from our HTML above
-  var canvas = document.querySelector("#renderCanvas");
+    var canvas = document.querySelector("#renderCanvas");
 
-  // Load the BABYLON 3D engine
-  var engine = new BABYLON.Engine(canvas, true);
+    // Load the BABYLON 3D engine
+    var engine = new BABYLON.Engine(canvas, true);
+    var createScene = function () {
+    var particleSystem = new BABYLON.ParticleSystem("particles", 2000, scene, customEffect);
+        particleSystem.particleTexture = new BABYLON.Texture("Flare.png", scene);
+    }
