027a1861f74f5cf1e3abcaa3aeb4a222e28efde3
Alexander Forselius <drsounds@gmail.com>
2026-06-27 16:33:03 +0200

    Working on bridge

--- src/main/java/se/spacify/controls/TabButton.java
+++ src/main/java/se/spacify/controls/TabButton.java
@@ -9,7 +9,6 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
