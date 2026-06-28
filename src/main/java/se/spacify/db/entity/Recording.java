edaccb46d40062943a15d19fcd3b7315edc5a32b
Alexander Forselius <drsounds@gmail.com>
2026-06-28 13:04:06 +0200

    Implementing abstraction

--- src/main/java/se/spacify/db/entity/Recording.java
+++ src/main/java/se/spacify/db/entity/Recording.java
@@ -1,8 +1,7 @@
 package se.spacify.db.entity;
 
 import com.j256.ormlite.field.DatabaseField;
-import com.j256.ormlite.field.ForeignCollectionField;
-import com.j256.ormlite.dao.ForeignCollection;
+
 import com.j256.ormlite.table.DatabaseTable;
 
 /**
